ef8d3b9b5eb21da1383b57cad30f58deb7998f08
Yu Zen <cheahyuzen@gmail.com>
2025-06-19 18:45:56 +0800

    added all snaps until 19 and pushing to github, but moving them to a thumbdrive from local repo cos its just too big a folder, gd luck future me labelling ratings 15-19 is necessary so dc abt feelings just get to it haha

--- GCloud/rater.py
+++ GCloud/rater.py
@@ -1,7 +1,7 @@
 import os
 
-for x in range(8, 15):
-    path = f'snaps_{x}'
+for x in range(15, 20): # to 19, 20 not counted in 'range'
+    path = fr'E:\snaps_{x}'
     filenames = os.listdir(path)
     writeto = f'rating_{x}.txt'
     with open(writeto, 'a') as file:
