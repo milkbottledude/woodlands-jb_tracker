dad2f6792f0f1422f0380b6c688f9a4c6c1dbe8c
Yu Zen <cheahyuzen@gmail.com>
2025-06-28 16:25:58 +0800

    added ratings 20-26 as well as snaps 20-26, have not yet implemented ratings 15-19 to ML model in GCloud webapp yet, imma do that now

--- GCloud/rater.py
+++ GCloud/rater.py
@@ -1,9 +1,9 @@
 import os
 
-for x in range(15, 20): # to 19, 20 not counted in 'range'
-    path = fr'E:\snaps_{x}'
+for x in range(20, 27): # to 19, 20 not counted in 'range'
+    path = f'E:\snaps_{x}'
     filenames = os.listdir(path)
-    writeto = f'rating_{x}.txt'
+    writeto = f'C:\Users\cheah\OneDrive\Documents\Coding\Project-JBridge\GCloud\rating_{x}.txt'
     with open(writeto, 'a') as file:
         for filename in filenames:
             file.write(filename + ' \n')
--- python_scripts/frombucket.py
+++ python_scripts/frombucket.py
@@ -1,8 +1,11 @@
 from google.cloud import storage
 import os
 
-# creds
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Yu Zen\Documents\Coding\sapient-metrics-436909-v6-09e1f1ee28c1.json"
+# creds for laptop
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Yu Zen\Documents\Coding\sapient-metrics-436909-v6-09e1f1ee28c1.json"
+# for desktop
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\cheah\OneDrive\Documents\Coding\sapient-metrics-436909-v6-09e1f1ee28c1.json"
+
 
 # downloading stuff in folder from bucket
 def download_folder(bucket_name, folder_name, local_destination):
@@ -24,7 +27,10 @@ def download_folder(bucket_name, folder_name, local_destination):
 
 # Example usage
 bucket_name = "frickubucket"
-folder_name = "towardsbkesnapshot/"  # Ensure it ends with a slash
-local_destination = r"C:\Users\Yu Zen\Documents\Coding\Project-JBridge\GCloud\bkesnaps"  # Path to folder
+folder_name = "snapshots/"  # Ensure it ends with a slash
+# Path to laptop folder
+# local_destination = r"C:\Users\Yu Zen\Documents\Coding\Project-JBridge\GCloud\blahblah"
+# to dekstop folder
+local_destination = r"E:\05_05_11_snaps_n_more"
 
 download_folder(bucket_name, folder_name, local_destination)
